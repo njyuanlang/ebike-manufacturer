f144486905ab7da1f8b02bcec7faa300c740b3fc
Guan Bo <guanbo2002@gmail.com>
2015-06-23 21:04:03 +0800

    Update: apt for open qq

---
+++ app/js/download.js
@@ -0,0 +1,8 @@
+// alert(navigator.userAgent)
+// location = "http://a.app.qq.com/o/simple.jsp?pkgname=com.extensivepro.ebike";
+// if ((navigator.userAgent.match(/iPhone/i)) || (navigator.userAgent.match(/iPod/i))) {
+//   location.replace("https://itunes.apple.com/cn/app/bang-da-shi/id960902581?mt=8#a.app.qq.com");
+// } else {
+//   location.replace("http://a.app.qq.com/o/simple.jsp?pkgname=com.extensivepro.ebike");
+// }
+location = "http://fusion.qq.com/app_download?appid= 1104658877&platform=qzone&via=QZ.MOBILEDETAIL.QRCODE&u=3046917960"
